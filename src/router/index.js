f83769a2d3c9b32ae25ccf810005b1452a16c898
Come Le Sauter <come.l.s@hotmail.fr>
2023-05-31 18:14:35 +0200

    css update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import TodosView from "../views/TodosView.vue";
-import AboutView from "../views/AboutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,11 +8,6 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: TodosView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: AboutView,
     },
   ],
 });
